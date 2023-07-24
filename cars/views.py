8efec7a67f1c8bb79d7566c7cca8fc2b60c79a92
alejandro <ale_demer@hotmail.com>
2023-07-24 16:33:43 -0300

    filtros y abm completo.

--- cars/views.py
+++ cars/views.py
@@ -14,6 +14,25 @@ class CarListView(ListView):
     context_object_name = "cars"
     paginate_by = 20
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.GET.get("category")
+        order_by = self.request.GET.get("order_by")
+
+        if category:
+            queryset = queryset.filter(category=category)
+
+        if order_by == "menor_precio":
+            queryset = queryset.order_by("price")
+        elif order_by == "mayor_precio":
+            queryset = queryset.order_by("-price")
+        elif order_by == "mas_viejo":
+            queryset = queryset.order_by("-year")
+        elif order_by == "mas_nuevo":
+            queryset = queryset.order_by("year")
+
+        return queryset
+
 
 @method_decorator(login_required, name="dispatch")
 class CarCreateView(CreateView):
@@ -33,5 +52,5 @@ class CarUpdateView(UpdateView):
 @method_decorator(login_required, name="dispatch")
 class CarDeleteView(DeleteView):
     model = Car
-    success_url = "/car"
-    template_name = "car/car_confirm_delete.html"
+    success_url = "/cars"
+    template_name = "cars/car_confirm_delete.html"
