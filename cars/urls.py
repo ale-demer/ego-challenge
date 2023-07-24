dda1b856ca69ce3c14e0e16df66c762ff68dce48
alejandro <ale_demer@hotmail.com>
2023-07-24 13:10:40 -0300

    estilos y scripts iniciales.

--- cars/urls.py
+++ cars/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CarCreateView, CarUpdateView, CarListView
+from .views import CarCreateView, CarUpdateView, CarListView, CarDeleteView
 
 urlpatterns = [
     path("", CarListView.as_view(), name="car_list"),
     path("add/", CarCreateView.as_view(), name="car_add"),
     path("<int:pk>/", CarUpdateView.as_view(), name="car_change"),
+    path("delete/<int:pk>/", CarDeleteView.as_view(), name="car_delete"),
 ]
